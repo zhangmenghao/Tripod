eaf0c6b3949edb35204aa7c20adff9fabf0888ac
zhangmenghao <zhangmenghao0503@gmail.com>
2017-12-19 11:44:06 +0800

    nf test

--- gateway/gateway.c
+++ gateway/gateway.c
@@ -283,7 +283,7 @@ lcore_nf(__attribute__((unused)) void *arg)
 				continue;
 
 			
-			//const uint16_t nb_tx = rte_eth_tx_burst(port ^ 1, 0, bufs, nb_rx);
+			const uint16_t nb_tx = rte_eth_tx_burst(port , 0, bufs, nb_rx);
 
 			for (i = 0; i < nb_rx; i ++){
 				printf("packet comes from %u\n", port);
@@ -312,11 +312,11 @@ lcore_nf(__attribute__((unused)) void *arg)
 			}
 
 			/* Free any unsent packets. */
-			/*if (unlikely(nb_tx < nb_rx)) {
+			if (unlikely(nb_tx < nb_rx)) {
 				uint16_t buf;
 				for (buf = nb_tx; buf < nb_rx; buf++)
 					rte_pktmbuf_free(bufs[buf]);
-			}*/
+			}
 		}
 	}
 	return 0;
