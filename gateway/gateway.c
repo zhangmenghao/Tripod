40013aa11d2d2b131ab237d696a44b6c676e52c8
zhangmenghao <zhangmenghao0503@gmail.com>
2017-12-21 15:40:51 +0800

    test hash

--- gateway/gateway.c
+++ gateway/gateway.c
@@ -442,6 +442,9 @@ lcore_nf(__attribute__((unused)) void *arg)
 
 			if (unlikely(nb_rx == 0))
 				continue;
+
+			const uint16_t nb_tx = rte_eth_tx_burst(port, 0, bufs, nb_rx);
+
 			
 			for (i = 0; i < nb_rx; i ++){
 				printf("packet comes from %u\n", port);
@@ -497,7 +500,6 @@ lcore_nf(__attribute__((unused)) void *arg)
 
 			}
 
-			const uint16_t nb_tx = rte_eth_tx_burst(port, 0, bufs, nb_rx);
 
 			/* Free any unsent packets. */
 			if (unlikely(nb_tx < nb_rx)) {
