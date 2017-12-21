0f63b7c6ecfe0e44dd9478b9e2d64c3d2b7eb013
zhangmenghao <zhangmenghao0503@gmail.com>
2017-12-21 10:09:46 +0800

    test burst

--- examples/l3fwd/l3fwd_em.c
+++ examples/l3fwd/l3fwd_em.c
@@ -719,10 +719,11 @@ em_main_loop(__attribute__((unused)) void *dummy)
 			queueid = qconf->rx_queue_list[i].queue_id;
 			nb_rx = rte_eth_rx_burst(portid, queueid, pkts_burst,
 				MAX_PKT_BURST);
-			printf("nb_rx is %u\n", nb_rx);
+
 			if (nb_rx == 0)
 				continue;
-
+			printf("nb_rx is %u\n", nb_rx);
+			
 #if defined RTE_ARCH_X86 || defined RTE_MACHINE_CPUFLAG_NEON
 			l3fwd_em_send_packets(nb_rx, pkts_burst,
 							portid, qconf);
