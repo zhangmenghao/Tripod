72be096102e389d343e40f6a9293a1d3bf58117b
ZhangMenghao <zhangmenghao0503@gmail.com>
2018-01-11 06:46:11 +0800

    modify go.sh for pktgen version 2.0

--- pktgen/version2/go.sh
+++ pktgen/version2/go.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-make; sudo ./build/pktgen -l 5 -n 3 -b 82:00.0 --proc-type auto --socket-mem 512,512 --file-prefix bf -- -p 0x1
+
+make; sudo ./build/pktgen -l 5 -n 3 --proc-type auto --socket-mem 512,512 --file-prefix bf -- -p 0x1
 
