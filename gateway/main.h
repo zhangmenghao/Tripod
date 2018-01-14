9714c6911423a7f1752fd7bef6bea8dcf0779464
Dracula <dracula.register@gmail.com>
2018-01-14 11:46:49 +0800

    Modify output

--- gateway/main.h
+++ gateway/main.h
@@ -122,6 +122,9 @@ extern struct ether_addr interface_MAC;
 extern uint32_t broadcast_ip;
 extern uint32_t this_machine_index;
 
+extern uint32_t flow_counts;
+extern uint32_t malicious_packet_counts;
+
 void convert_ipv4_5tuple(struct ipv4_5tuple *key1, union ipv4_5tuple_host *key2);
 void setStates(struct ipv4_5tuple *ip_5tuple, struct nf_states *state);
 int getStates(struct ipv4_5tuple *ip_5tuple, struct nf_states ** state);
